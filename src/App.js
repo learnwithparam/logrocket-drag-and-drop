96ee3cfba578354204e22510f2dbd1f74c38013d
Param Harrison <paramanantham@live.com>
2019-08-25 21:28:09 +0300

    feat: separate dropzone logic from App.css

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
 import React, { useCallback, useState } from "react";
+import Dropzone from "./Dropzone";
 import "./App.css";
-import { useDropzone } from "react-dropzone";
-
-const getClassName = (className, isActive) => {
-  if (!isActive) return className;
-  return `${className} ${className}-active`;
-};
-
-const MyDropzone = ({ onDrop, accept }) => {
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop,
-    accept
-  });
-
-  return (
-    <div className={getClassName("dropzone", isDragActive)} {...getRootProps()}>
-      <input className="dropzone-input" {...getInputProps()} />
-      <div className="text-center">
-        {isDragActive ? (
-          <p className="dropzone-content">Drop the files here ...</p>
-        ) : (
-          <p className="dropzone-content">
-            Drag 'n' drop some files here, or click to select files
-          </p>
-        )}
-      </div>
-    </div>
-  );
-};
 
 function App() {
   const [images, setImages] = useState([]);
@@ -45,7 +18,7 @@ function App() {
   return (
     <main className="App">
       <h1 className="text-center">Drag and Drop</h1>
-      <MyDropzone onDrop={onDrop} accept={"image/*"} />
+      <Dropzone onDrop={onDrop} accept={"image/*"} />
       {images && images.length > 0 && (
         <h3 className="text-center">Drag the Images to change positions</h3>
       )}
