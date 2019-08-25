27579972032e5700c110d6d75b1f436ecf9f4f56
Paramanantham Harrison <paramanantham@live.com>
2019-08-25 21:29:03 +0300

    feat: separate dropzone component (#3)

---
+++ src/Dropzone.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDropzone } from "react-dropzone";
+
+const getClassName = (className, isActive) => {
+  if (!isActive) return className;
+  return `${className} ${className}-active`;
+};
+
+const Dropzone = ({ onDrop, accept }) => {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept
+  });
+
+  return (
+    <div className={getClassName("dropzone", isDragActive)} {...getRootProps()}>
+      <input className="dropzone-input" {...getInputProps()} />
+      <div className="text-center">
+        {isDragActive ? (
+          <p className="dropzone-content">Drop the files here ...</p>
+        ) : (
+          <p className="dropzone-content">
+            Drag 'n' drop some files here, or click to select files
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dropzone;
